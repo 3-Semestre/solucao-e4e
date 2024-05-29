c3e5a79afda1ea4b7bc5df898f920d55791aab49
Miguel Silva <gisele.silva@sptech.school>
2024-05-29 20:16:21 -0300

    nav bar & form professor c/ padrao bootstrap

--- english4ever/src/App.js
+++ english4ever/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Componentes/Sidebar/Sidebar';
+import Breadcrumbs from './Componentes/Breadcrump/Breadcrumb';
+import BoxProfile from './Componentes/BoxProfile/BoxProfile';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <div className='teste'>
+        Essa vai ser a div de conteúdo principal
+      </div>
+        <BoxProfile />
     </div>
   );
 }
