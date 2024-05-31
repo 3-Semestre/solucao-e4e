b17bf967c25983f31981fb189353a9c8e23ada0d
Miguel Silva <gisele.silva@sptech.school>
2024-05-31 00:06:35 -0300

    tela cadastro professor terminada

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-btn').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('active');
+    document.querySelector('.conteudo').classList.toggle('active');
+});
