f3d5dc7a121a497e18c23a0bc28edd8bd9741e73
Miguel-Araujo325 <miguel.asilva@sptech.school>
2024-07-24 05:27:38 -0300

    nivel e nicho dinamico

--- js/exibirPerfil.js
+++ js/exibirPerfil.js
@@ -1,7 +1,8 @@
+const id = sessionStorage.getItem('id')
+const nivel_acesso_cod = sessionStorage.getItem('nivel_acesso_cod')
+
 async function exibirDadosPerfil() {
     const nivel_acesso = sessionStorage.getItem('nivel_acesso').toLowerCase();
-
-    const id = sessionStorage.getItem('id')
 
     const resposta = await fetch(`http://localhost:8080/usuarios/perfil/${nivel_acesso}/${id}`);
     if (!resposta.ok) {
@@ -13,11 +14,11 @@ async function exibirDadosPerfil() {
     if (!dados || dados.length === 0) {
         throw new Error('Dados não encontrados');
     }
-  
+
     preencherInput(dados);
 }
 
-function preencherInput(dados){
+function preencherInput(dados) {
     var nomeTitulo = document.getElementById("nome_titulo");
     var nome = document.getElementById("nome");
     var nomeInput = document.getElementById("input_nome");
@@ -61,35 +62,108 @@ function preencherInput(dados){
     }
 }
 
-async function buscarNivelInglesUsuario() {
-    const id = sessionStorage.getItem('id')
+function tratarNome(nichoNome) {
+    let nomeTratado = nichoNome.replace(/_/g, ' ');
 
+    let palavras = nomeTratado.split(' ');
+
+    palavras = palavras.map(palavra => {
+        return palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase();
+    });
+
+    nomeTratado = palavras.join(' ');
+
+    return nomeTratado;
+}
+
+async function buscarNivelIngles() {
+    const resposta = await fetch("http://localhost:8080/nivel-ingles");
+    const listaNiveis = await resposta.json();
+    const checkboxList = document.getElementById("nivelCheckboxList");
+
+    checkboxList.innerHTML = "";
+
+    if (nivel_acesso_cod != 3) {
+        const checkboxes = listaNiveis.map(nivel => `
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="nivel" value="${nivel.id}" id="nivel_${nivel.id}">
+                    <label class="form-check-label" for="nivel_${nivel.id}">${tratarNome(nivel.nome)}</label>
+                </div>
+            `).join('');
+        checkboxList.innerHTML += checkboxes;
+    } else {
+        const options = listaNiveis.map((nivel) => `
+                <option value="${nivel.id}" id="nivel_${nivel.id}">${tratarNome(nivel.nome)}</option>
+            `).join('');
+        checkboxList.innerHTML += `
+                <label class="form-label" id="nivelTitle"><span>*</span>Nível de Inglês:</label>
+                <select class="form-select" id="nivel" aria-label="Default select example" required>
+                    <option value="" selected>Selecione uma opção</option>
+                    ${options}
+                </select>
+            `;
+    }
+}
+
+
+async function buscarNichos() {
+    const resposta = await fetch("http://localhost:8080/nichos");
+    const listaNichos = await resposta.json();
+    const checkboxList = document.getElementById("nichoCheckboxList");
+
+    if (nivel_acesso_cod != 3) {
+        const checkboxes = listaNichos.map(nicho => `
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="nicho" value="${nicho.id}" id="nicho_${nicho.id}">
+                <label class="form-check-label" for="nicho_${nicho.id}">${tratarNome(nicho.nome)}</label>
+                </div>
+            `).join('');
+        checkboxList.innerHTML += checkboxes;
+    } else {
+        const options = listaNichos.map((nicho) => `
+                <option value="${nicho.id}" id="nivel_${nicho.id}">${tratarNome(nicho.nome)}</option>
+            `).join('');
+        checkboxList.innerHTML += `
+                <select class="form-select" id="nicho" aria-label="Default select example" required>
+                    <option value="" selected>Selecione uma opção</option>
+                    ${options}
+                </select>
+            `;
+    }
+}
+
+
+async function buscarNivelInglesUsuario() {
     const resposta = await fetch(`http://localhost:8080/usuario-nivel-ingles/usuario/${id}`);
 
     const respostaNivel = await resposta.json();
 
-    var nivelIngles = document.getElementById("nivel");
-
-    for (let i = 0; i < respostaNivel.length; i++) {
-        if (i === respostaNivel.length - 1) {
-            nivelIngles.innerHTML += respostaNivel[i].nivelIngles.nome;
-        } else {
-            nivelIngles.innerHTML += respostaNivel[i].nivelIngles.nome + ", ";
+    console.log(respostaNivel)
+    if (nivel_acesso_cod != 3) {
+        var nivelIngles = document.getElementById("nivel");
+    
+        for (let i = 0; i < respostaNivel.length; i++) {
+            if (i === respostaNivel.length - 1) {
+                nivelIngles.innerHTML += respostaNivel[i].nivelIngles.nome;
+            } else {
+                nivelIngles.innerHTML += respostaNivel[i].nivelIngles.nome + ", ";
+            }
         }
+    
+        respostaNivel.forEach((nivelUsuario) => {
+            const checkbox = document.getElementById(`nivel_${nivelUsuario.nivelIngles.id}`);
+            if (checkbox) {
+                checkbox.checked = true;
+            }
+        });
+    } else{
+        const nivel_ingles_input = document.getElementById("nivel");
     }
-
-    respostaNivel.forEach((nivelUsuario) => {
-        const checkbox = document.getElementById(`nivel_${nivelUsuario.nivelIngles.id}`);
-        if (checkbox) {
-            checkbox.checked = true;
-        }
-    });
-
+        
+    
 }
 
 async function buscarNichoUsuario() {
-    const id = sessionStorage.getItem('id');
-
     const resposta = await fetch(`http://localhost:8080/usuario-nicho/usuario/${id}`);
     const respostaNicho = await resposta.json();
     const nicho = document.getElementById("nicho");
@@ -115,56 +189,11 @@ async function buscarNichoUsuario() {
     });
 }
 
-function tratarNome(nichoNome) {
-    let nomeTratado = nichoNome.replace(/_/g, ' ');
-
-    let palavras = nomeTratado.split(' ');
-
-    palavras = palavras.map(palavra => {
-        return palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase();
-    });
-
-    nomeTratado = palavras.join(' ');
-
-    return nomeTratado;
-}
-
-async function buscarNivelIngles() {
-    const resposta = await fetch("http://localhost:8080/nivel-ingles");
-    const listaNiveis = await resposta.json();
-    const checkboxList = document.getElementById("nivelCheckboxList");
-
-    listaNiveis.forEach((nivel) => {
-        const checkbox = `
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="nivel" value="${nivel.id}" id="nivel_${nivel.id}">
-                <label class="form-check-label" for="nivel_${nivel.id}">${tratarNome(nivel.nome)}</label>
-            </div>
-        `;
-        checkboxList.innerHTML += checkbox;
-    });
-}
-
-async function buscarNichos() {
-    const resposta = await fetch("http://localhost:8080/nichos");
-    const listaNichos = await resposta.json();
-    const checkboxList = document.getElementById("nichoCheckboxList");
-
-    listaNichos.forEach((nicho) => {
-        const checkbox = `
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="nicho" value="${nicho.id}" id="nicho_${nicho.id}">
-                <label class="form-check-label" for="nicho_${nicho.id}">${tratarNome(nicho.nome)}</label>
-            </div>
-        `;
-        checkboxList.innerHTML += checkbox;
-    });
-}
 
 window.onload = function () {
     exibirDadosPerfil();
     buscarNivelIngles();
     buscarNichos();
     buscarNivelInglesUsuario();
-    buscarNichoUsuario();
+    //buscarNichoUsuario();
 };
