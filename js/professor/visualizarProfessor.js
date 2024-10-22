ca2bcb4cabe32e4e21c5b76265e7df6351ff7f9d
Miguel-Araujo325 <miguel.asilva@sptech.school>
2024-10-22 13:19:27 -0300

    metas

--- js/professor/visualizarProfessor.js
+++ js/professor/visualizarProfessor.js
@@ -2,13 +2,12 @@ var paginaAtual = 0;
 var totalPaginas = 0;
 
 const id = sessionStorage.getItem('id')
-const nivel_acesso_cod = sessionStorage.getItem('nivel_acesso_cod')
 const token = sessionStorage.getItem('token')
 
 async function buscarProfessor(paginaAtual) {
     if (paginaAtual < 0 || (totalPaginas > 0 && paginaAtual >= totalPaginas)) return; // Limita as páginas
 
-    const cardsProfessor = document.getElementById("listagem_usuarios")
+    const cardsProfessor = document.getElementById("listagem_usuarios");
 
     const resposta = await fetch(`http://localhost:8080/usuarios/professor/paginado?page=${paginaAtual}`, {
         method: 'GET',
@@ -19,126 +18,87 @@ async function buscarProfessor(paginaAtual) {
     });
 
     if (resposta.status == 204) {
-        cardsProfessor.innerHTML += "<span>Não há professores cadastrados...<span> <br/>Cadastre um novo clicando <a href='cadastrar.html?tipo=professor'>aqui</a>"
-        return
+        cardsProfessor.innerHTML += "<span>Não há professores cadastrados...<span> <br/>Cadastre um novo clicando <a href='cadastrar.html?tipo=professor'>aqui</a>";
+        return;
     }
 
     const listaProfessors = await resposta.json();
 
     cardsProfessor.innerHTML = "";
     cardsProfessor.innerHTML += listaProfessors.content.map((professor) => {
+        const professorId = professor.id; // ID do professor para garantir unicidade
+
         return `
-      <div class="dados-student" id="card_dados">
-    <div class="header-student">
-        <img src="../imgs/perfil_blue.png" alt="Foto do professor">
-        <p>${professor.nome_completo}</p>
-    </div>
-    <br/>
-    <br/>
-    <div class="form-student">
-        <div class="personal-information">
-            <div class="form-group">
-                <label for="cpf">CPF:</label>
-                <label class="label2" type="text" id="cpf"> ${professor.cpf} </label>
+      <div class="dados-student" id="card_dados_${professorId}">
+        <div class="header-student">
+            <img src="../imgs/perfil_blue.png" alt="Foto do professor">
+            <p>${professor.nome_completo}</p>
+        </div>
+        <br/>
+        <br/>
+        <div class="form-student">
+            <div class="personal-information">
+                <div class="form-group">
+                    <label for="cpf_${professorId}">CPF:</label>
+                    <label class="label2" type="text" id="cpf_${professorId}"> ${professor.cpf} </label>
+                </div>
+                <div class="form-group">
+                    <label for="data-nascimento_${professorId}">Data de Nascimento:</label>
+                    <label class="label2" type="date" id="data-nascimento_${professorId}"> ${formatarData(professor.data_nascimento)} </label>
+                </div>
+                <div class="form-group">
+                    <label for="email_${professorId}">E-mail:</label>
+                    <label class="label2" type="email" id="email_${professorId}"> ${professor.email} </label>
+                </div>
+                <div class="form-group">
+                    <label for="telefone_${professorId}">Telefone:</label>
+                    <label class="label2" type="text" id="telefone_${professorId}"> ${formatarCelular(professor.telefone)} </label>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="data-nascimento">Data de Nascimento:</label>
-                <label class="label2" type="date" id="data-nascimento"> ${formatarData(professor.data_nascimento)} </label>
-            </div>
-            <div class="form-group">
-                <label for="email">E-mail:</label>
-                <label class="label2" type="email" id="email"> ${professor.email} </label>
-            </div>
-            <div class="form-group">
-                <label for="telefone">Telefone:</label>
-                <label class="label2" type="text" id="telefone"> ${formatarCelular(professor.telefone)} </label>
+
+            <div class="course-information">
+                <div class="form-group">
+                    <label for="nivel-ingles_${professorId}">Nível de Inglês:</label>
+                    <label class="label2" type="text" id="nivel-ingles_${professorId}"> ${professor.niveis_Ingles} </label>
+                </div>
+                <div class="form-group">
+                    <label for="nicho_${professorId}">Nicho:</label>
+                    <label class="label2" type="text" id="nicho_${professorId}"> ${tratarNome(professor.nichos) || ''} </label>
+                </div>
+                <div class="form-group">
+                    <label for="horario-trabalho_${professorId}">Horário de trabalho:</label>
+                    <label class="label2" type="text" id="horario-trabalho_${professorId}">${formatarHorario(professor.inicio || '')} às ${formatarHorario(professor.fim || '')} </label>
+                </div>
+                <div class="form-group">
+                    <label for="horario-intervalo_${professorId}">Horário de intervalo:</label>
+                    <label class="label2" type="text" id="horario-intervalo_${professorId}">${formatarHorario(professor.pausa_inicio || '')} às ${formatarHorario(professor.pausa_fim || '')} </label>
+                </div>
+                <div class="form-group">
+                    <label for="meta_${professorId}">Metas:</label>
+                    <input class="label2" type="text" id="meta_${professorId}" value="toDo" readonly>
+                </div>
             </div>
         </div>
 
-        <div class="course-information">
-            <div class="form-group">
-                <label for="nivel-ingles">Nível de Inglês:</label>
-                <label class="label2" type="text" id="nivel-ingles"> ${professor.niveis_Ingles} </label>
+        ${nivelAcesso === 3 ? `
+            <div class="lapis">
+                <img src="../imgs/pen.png" alt="Editar professor" style="width: 3vw; height: 6vh" onclick="editarProfessor(${professorId})">
             </div>
-            <div class="form-group">
-                <label for="nicho">Nicho:</label>
-                <label class="label2" type="text" id="nicho"> ${tratarNome(professor.nichos) || ''} </label>
+            <div class="lixeira" onclick="confirmacaoDeleteProfessor(${professorId})">
+                <img src="../imgs/trash-bin.png" alt="Excluir professor" style="width: 3vw; height: 6vh">
             </div>
-            <div class="form-group">
-                <label for="nicho">Horário de trabalho:</label>
-                <label class="label2" type="text">${formatarHorario(professor.inicio || '')} às ${formatarHorario(professor.fim || '')} </label>
-            </div>
-            <div class="form-group">
-                <label for="nicho">Horário de intervalo:</label>
-                <label class="label2" type="text">${formatarHorario(professor.pausa_inicio || '')} às ${formatarHorario(professor.pausa_fim || '')} </label>
-            </div>
-        </div>
-    </div>
-    <div class="lapis" onclick="confirmacaoDeleteAluno(${professor.id})">
-        <img src="../imgs/pen.png" alt="Excluir aluno"  style="width: 3vw; height: 6vh">
-    </div>
-    <div class="lixeira" onclick="confirmacaoDeleteProfessor(${professor.id})">
-        <img src="../imgs/trash-bin.png" alt="Excluir professor">
-    </div>
-</div>
-<hr class="line">`
+            ` : ''}
+      </div>
+      <hr class="line">
+    `;
     }).join('');
 
-    atualizarBotoesPaginacaoProfessor(listaProfessors.totalPages, listaProfessors.pageable.pageNumber)
+    atualizarBotoesPaginacaoProfessor(listaProfessors.totalPages, listaProfessors.pageable.pageNumber);
 
+    // Esconde o GIF de carregamento
     const loadingGif = document.getElementById('loading');
-    const tabela = document.getElementById("tabela_agendamento");
-    const tempoMinimoCarregamento = 700; // 1 segundo (1000 ms) de tempo mínimo de carregamento
-
-    // Mostrar o GIF de carregamento
-    loadingGif.style.display = 'block';
-
-    // Função para simular o tempo de carregamento mínimo
-    const esperarMinimoCarregamento = new Promise(resolve => setTimeout(resolve, tempoMinimoCarregamento));
-
-    try {
-        if (pagina < 0 || (totalPaginas > 0 && pagina >= totalPaginas)) return; // Limita as páginas
-
-        const resposta = await fetch(`http://localhost:8080/agendamento/historico/${id}?page=${pagina}&tempo=${tempo}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!resposta.ok) {
-            throw new Error('Erro ao buscar dados do servidor');
-        } else if (resposta.status == 204) {
-            tabela.innerHTML = 'Não há agendamentos registrados';
-            return;
-        }
-
-        const dados = await resposta.json();
-
-        if (!dados || dados.content.length === 0) {
-            throw new Error('Dados não encontrados');
-        }
-
-        totalPaginas = dados.totalPages;
-
-        limparTabela();
-        preencherTabela(dados.content);
-        atualizarBotoesPaginacao(dados.totalPages, dados.pageable.pageNumber);
-    } catch (error) {
-        console.error(error.message);
-        tabela.innerHTML = 'Erro ao carregar agendamentos';
-    } finally {
-        // Garantir que o tempo mínimo de carregamento foi respeitado antes de esconder o GIF
-        await Promise.all([esperarMinimoCarregamento]);
-
-        // Esconder o GIF de carregamento
-        loadingGif.style.display = 'none';
-    }
-
+    loadingGif.style.display = 'none';
 }
-
-
 
 function confirmacaoDeleteProfessor(id) {
     Swal.fire({
@@ -187,6 +147,14 @@ async function deletarProfessor(id) {
             footer: '<a href="mailto:support@eduivonatte.com">Precisa de ajuda? Clique aqui para enviar um e-mail para o suporte.</a>',
             timer: 2000
         });
+    }
+}
+
+function editarProfessor(id) {
+    const input = document.getElementById(`meta_${id}`);
+    if (input) {
+        input.removeAttribute("readonly"); 
+        input.focus(); 
     }
 }
 
